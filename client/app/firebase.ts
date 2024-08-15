aaba1e1630b3bfafff8d8577deff438bc4cb5ae4
Benson Manzano <bensonfmanzano@gmail.com>
2024-08-15 12:15:24 -0700

    Added changes to configuration with using typescript, still need work on making the delete member function worK

---
+++ client/app/firebase.ts
@@ -0,0 +1,64 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, getDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_DB_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_DB_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_DB_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_DB_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_DB_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_DB_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_DB_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+interface BoardMember {
+  name: string;
+  id: string;
+}
+
+async function fetchBoardMembers(): Promise<BoardMember[]> {
+  const querySnapshot = await getDocs(collection(db, "BoardMembers"));
+  const boardMembers: BoardMember[] = [];
+
+  querySnapshot.forEach((doc) => {
+    boardMembers.push({
+      name: doc.data().Name,
+      id: doc.id, // Set id directly from doc.id
+    });
+  });
+
+  console.log(boardMembers)
+  return boardMembers;
+}
+
+async function addBoardMember({ name }: {name : string}) {
+  try {
+    const docRef = await addDoc(collection(db, "BoardMembers"), {
+      name,
+    });
+    console.log("Document written with ID: ", docRef.id);
+    return docRef; // Return the document reference with the generated id
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    throw e;
+  }
+}
+
+const removeBoardMember = async (name : string) =>{
+  try {
+    const docRef = doc(db, "BoardMembers", name)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap) {
+      await deleteDoc(docRef);
+    }
+
+  } catch (e) {
+    console.error("Error deleting document", name);
+  }
+}
+
+export { app, db, fetchBoardMembers, addBoardMember, removeBoardMember };
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,21 +1,79 @@
 "use client";
 
-import React, { useEffect } from 'react';
-import { fetchBoardMembers} from './firebase';
+import React, { useState, useEffect } from "react";
+import {
+  fetchBoardMembers,
+  addBoardMember,
+  removeBoardMember,
+} from "./firebase";
+
+interface BoardMember {
+  name: string;
+  id: string;
+}
 
 export default function Home() {
+  const [name, setName] = useState<string>("");
+  const [nameDelete, setNameDelete] = useState("");
+  const [boardMembers, setBoardMembers] = useState<BoardMember[]>([]);
+
   useEffect(() => {
-    // Fetch initial data
-    fetchBoardMembers();
-    
-    // Subscribe to real-time updates
+    fetchBoardMembers()
+      .then((boardMembers: BoardMember[]) => setBoardMembers(boardMembers))
+      .catch((error) => console.error("Error fetching board members:", error));
   }, []);
-  
+
+  const handleAddMember = async () => {
+    if (name) {
+      try {
+        const docRef = await addBoardMember({ name });
+        setBoardMembers((prevMembers) => [
+          ...prevMembers,
+          { id: docRef.id, name },
+        ]);
+        setName("");
+      } catch (error) {
+        console.error("Error adding board member:", error);
+      }
+    }
+  };
+
+  const handleDeleteMember = async (name: string) => {
+    try {
+      await removeBoardMember(name);
+      setName("");
+      setBoardMembers((prevMembers) => [...prevMembers]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <h1>Board Members</h1>
-      <button>Sign In</button>
+      <ul>
+        {boardMembers.map((member) => (
+          <li key={member.id}>{member.name}</li>
+        ))}
+      </ul>
+
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Add Member"
+        className="text-black"
+      />
+      <button onClick={handleAddMember}>Add Board Member</button>
+
+      <input
+        value={nameDelete}
+        onChange={(e) => setNameDelete(e.target.value)}
+        placeholder="Delete Member"
+        className={"text-black"}
+      />
+      <button onClick={() => handleDeleteMember(nameDelete)}>
+        Delete Member
+      </button>
     </div>
   );
 }
